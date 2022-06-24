101de7fcb429f9ad1eeca24ecb4ef7acc12c9e36
SoniaRodrigues <76952338+SoniaSibele@users.noreply.github.com>
2022-06-24 16:32:20 -0100

    Add files via upload

---
+++ translate.py
@@ -0,0 +1,30 @@
+
+from googletrans import Translator
+import googletrans
+translator = Translator()
+print(translator.translate('안녕하세요.'))
+print(translator.translate('안녕하세요.', dest='ja'))
+print(translator.translate('veritas lux mea', src='la', dest='pt'))
+
+
+# deteçao de linguagem
+translator = Translator()
+print(translator.detect('이 문장은 한글로 쓰여졌습니다.'))
+print(translator.detect('この文章は日本語で書かれました。'))
+print(translator.detect('This sentence is written in English.'))
+print(translator.detect('Tiu frazo estas skribita en Esperanto.'))
+print(translator.detect('Esta frase está escrita em Português'))
+
+#traduçao ingles portugues
+texto_en = input("Write a sentence: ")
+translator = Translator()
+texto_pt = translator.translate(texto_en, src="en", dest="pt")
+print("Original text:", texto_en)
+print("Translation:", texto_pt.text)
+
+#traduçao portugues ingles
+texto_pt = input("Escreva uma frase: ")
+translator = Translator()
+texto_en = translator.translate(texto_pt, src="pt", dest="en")
+print("Texto original:", texto_pt)
+print("Tradução:", texto_en.text)
